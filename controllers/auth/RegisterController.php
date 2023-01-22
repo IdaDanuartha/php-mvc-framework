e15c462e9ec8ea50319a2195326c179ab0c927cc
IdaDanuartha <idadanuartha42@smk.belajar.id>
2023-01-22 14:24:17 +0800

    add register model

--- controllers/auth/RegisterController.php
+++ controllers/auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace app\controllers\auth;
 use app\core\Application;
 use app\core\Controller;
 use app\core\Request;
+use app\models\RegisterModel;
 /**
  * Class RegisterController
  * 
@@ -16,7 +17,23 @@ class RegisterController extends Controller
 {
     public function index()
     {
+        $registerModel = new RegisterModel();
+        
+        if($request->isPost()) {
+            $registerModel->loadData($request->getBody);
+
+            if($registerModel->validate() && $registerModel->register()) {
+                return "Success";
+            }
+
+            return $this->render('auth/register', [
+                "model" => $registerModel
+            ]);
+        }
+
         $this->setLayout('auth');
-        return $this->render('auth/register');
+        return $this->render('auth/register', [
+            "model" => $registerModel
+        ]);
     }
 }
---
+++ models/RegisterModel.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace app\models;
+
+/**
+ * Class Controller
+ * 
+ * @author IdaDanuartha <danuart21@gmail.com>
+ * @package app/core
+ */
+
+class RegisterModel 
+{
+    public string $name;
+    public string $email;
+    public string $password;
+    public string $confirm_passowrd;
+}
